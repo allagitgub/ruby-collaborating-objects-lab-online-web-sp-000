842906fae3b054f51a9bbda684f41639e457fcd1
Alla Kuzminova <alchik@gmail.com>
2020-11-03 22:21:15 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(file)
+    song = Song.new(file)
+    str = file.delete_suffix(".mp3")
+    strArr = str.split("-")
+    song.name = strArr[1]
+    song.genre = strArr[2]
+    artist_name = strArr[0]
+    song
+  end
+
+  def artist_name=(name)
+    artist = find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+end
